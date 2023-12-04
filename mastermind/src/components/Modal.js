c6cac92d27b5175c35c4e4f2729853707ce886a3
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2023-12-04 11:22:47 -0600

    adding react

---
+++ mastermind/src/components/Modal.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+const Modal = () => {
+    
+return{
+<div class="modal" tabindex="-1" id="modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body text-center">
+        <h2 id="show-modal-text"></h2>
+      </div>
+      <div class="modal-footer">
+        <button
+          type="button"
+          class="btn btn-secondary"
+          data-bs-dismiss="modal"
+          onclick="closeModal()"
+        >
+          Close
+        </button>
+
+        <button type="button" class="btn btn-secondary" onclick="saveScore()">
+          Save Score
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+}
+}
+
+export default Modal;
